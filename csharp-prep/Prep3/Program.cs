19fc4064dfe15adb5b365c52274d1bdfe142e24d
rostislav-nikitin90 <rosnikitin90@gmail.com>
2024-06-11 17:10:18 +0200

    Completed C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 101);
+
+        int guessNum = -1;
+
+        while (guessNum != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            string userGuessNum = Console.ReadLine();
+            guessNum = int.Parse(userGuessNum);
+
+            if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
