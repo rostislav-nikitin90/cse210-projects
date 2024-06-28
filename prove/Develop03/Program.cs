ab5c1fff4fd494d895c07a3dc6a2b3ffb0f1cdcc
rostislav-nikitin90 <rosnikitin90@gmail.com>
2024-06-28 22:51:40 +0200

    Completed W03 Prove: Developer-Scripture Memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,36 @@
-using System;
+/* Exceeds requirements: The program works with a library of scriptures rather
+than a single one and chooses scriptures at random to present to the user. */
 
-class Program
+using System.Collections.Specialized;
+
+namespace Develop03;
+
+static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        ScriptureLibrary library = new ScriptureLibrary();
+        Scripture scripture = library.GetRandomScripture();
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.DisplayScripture());
+
+            if (scripture.IsCompletelyHidden()) break;
+
+            Console.Write("\nPress Enter to continue (hide a word) or type 'quit' to exit: ");
+            string userInput = Console.ReadLine();
+
+            if (userInput != null && userInput.ToLower() == "quit") break;
+            
+            if (string.IsNullOrEmpty(userInput))
+            {
+                scripture.HideRandomWords();
+                continue;
+            }
+            Console.Write("Wrong Command. You need to choose either to press Enter or type 'quit'!");
+            Thread.Sleep(3000);
+        }
     }
 }
